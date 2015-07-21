edb43b59602cc0da16acde63ce40d5072f1571ae
tokko <andreg@kth.se>
2015-07-21 09:25:39 +0200

    continuous delivery to alpha channel on google play

---
+++ app/src/test/java/com/eventtasks/tokko/eventtasks/Dummy.java
@@ -0,0 +1,15 @@
+package com.eventtasks.tokko.eventtasks;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class Dummy {
+
+    @Test
+    @Ignore
+    public void failingTest(){
+        fail();
+    }
+}
